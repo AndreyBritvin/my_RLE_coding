a8d7c669ba3aa423af6ac23e9eb142ef60e423c6
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-08-30 14:42:33 +0300

    Added new test file

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@ int main()
     // char my_str[] = "aaaabbbcdeggffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffddefgelkkfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfd";
 
     code_rle("text_to_compr/to_encode.txt", "text_to_compr/coded_text.hex");
-
-    // char my_str_2[] = "4a3b-3cde2g8f2d-5efgel2k";
-
     decode_rle("text_to_compr/coded_text.hex", "text_to_compr/decoded_text.txt");
+
+    code_rle("text_to_compr/long_to_encode.txt", "text_to_compr/lcoded_text.hex");
+    decode_rle("text_to_compr/lcoded_text.hex", "text_to_compr/long_decode.txt");
 
     code_rle("photo_ded/image.png", "photo_ded/coded_image.hex");
     decode_rle("photo_ded/coded_image.hex", "photo_ded/image_decode.png");
